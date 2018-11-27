893e183534afb1042560f1a3111a6bdfe4a786df
Zoltán Takács <zoltan.takacs@liferay.com>
2018-11-27 15:58:41 +0100

    COMMERCE-618 Add installer for the tLiferay component family

---
+++ modules/etl/talend/installer/go.mod
@@ -0,0 +1,16 @@
+module installer
+
+require (
+	bou.ke/monkey v1.0.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-ini/ini v1.39.0
+	github.com/gopherjs/gopherjs v0.0.0-20181103185306-d547d1d9531e // indirect
+	github.com/jtolds/gls v4.2.1+incompatible // indirect
+	github.com/otiai10/copy v0.0.0-20180813032824-7e9a647135a1
+	github.com/otiai10/mint v1.2.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/smartystreets/assertions v0.0.0-20180927180507-b2de0cb4f26d // indirect
+	github.com/smartystreets/goconvey v0.0.0-20181108003508-044398e4856c // indirect
+	github.com/stretchr/testify v1.2.2 // indirect
+	gopkg.in/ini.v1 v1.39.0 // indirect
+)
---
+++ modules/etl/talend/installer/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"installer/constants"
+	"installer/utils/fileutils"
+	"installer/verifier"
+)
+
+func main() {
+	displayInitializationMessages()
+
+	verifier.VerifyInstallerIntegrity()
+	verifier.VerifyTOSLocation()
+	verifier.VerifyTOSVersion()
+
+	// Copy the component artifacts
+	copyTasks := []fileutils.CopyTask{
+		{filepath.Join(
+			constants.ThirdPartyDependenciesDir, ".m2"), constants.TosM2Dir},
+		{filepath.Join(
+			constants.ComponentArtifactsDir, ".m2"), constants.TosM2Dir},
+		{fileutils.GetSrcComponentDefinitionPath(),
+			filepath.Join(
+				constants.TosHomeDir, "plugins",
+				fileutils.GetSrcComponentDefinitionName())},
+	}
+
+	for _, copyTask := range copyTasks {
+		err := copyTask.Execute()
+
+		if err != nil {
+			log.Printf("Unable to copy files for the component: %v", err)
+			log.Fatal("Installation failed")
+		}
+	}
+
+	// Delete the TOS Eclipse OSGi / temp files
+	files, err := ioutil.ReadDir(constants.TosM2HomeDir)
+	if err != nil || len(files) == 0 {
+		log.Printf("Unable to read Talend's configuration folder: %v", err)
+	}
+
+	for _, file := range files {
+		if file.IsDir() && strings.HasPrefix(file.Name(), "org.eclipse.") {
+			os.RemoveAll(filepath.Join(constants.TosM2HomeDir, file.Name()))
+		}
+	}
+
+	fmt.Println("\nInstallation of the Liferay component was successful!")
+}
+
+func displayInitializationMessages() {
+	const decorLine = "\t\t#################################################" +
+		"######################"
+
+	fmt.Println("\n" + decorLine)
+	fmt.Println(
+		"\t\t### Liferay component family installer for Talend Open Studio " +
+			constants.SUPPORTED_TOS_VERSION + " ###")
+	fmt.Println(decorLine + "\n")
+
+	fmt.Printf("Installer's location: \t\t\t%s\n",
+		constants.GetBinaryLocation())
+	fmt.Printf("Talend Open Studio's Home directory: \t%s\n",
+		constants.TosHomeDir)
+}
