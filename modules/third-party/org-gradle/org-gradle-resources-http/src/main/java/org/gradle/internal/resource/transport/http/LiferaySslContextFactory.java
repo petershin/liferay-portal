4ab3650c4203e8340633eef81c56c567591c8abf
Peter Shin <peter.shin@liferay.com>
2018-12-11 15:33:56 -0800

    LPS-88176 Add logging for SSLContext

--- modules/third-party/org-gradle/org-gradle-resources-http/src/main/java/org/gradle/internal/resource/transport/http/LiferaySslContextFactory.java
+++ modules/third-party/org-gradle/org-gradle-resources-http/src/main/java/org/gradle/internal/resource/transport/http/LiferaySslContextFactory.java
@@ -14,6 +14,15 @@
 
 package org.gradle.internal.resource.transport.http;
 
+import java.io.File;
+
+import java.net.URL;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import java.security.Security;
+
 import javax.net.ssl.SSLContext;
 
 import org.apache.http.ssl.SSLContexts;
@@ -36,7 +45,69 @@ public class LiferaySslContextFactory extends DefaultSslContextFactory {
 			_logger.info("Creating default SSL context from system properties");
 		}
 
-		return SSLContexts.createSystemDefault();
+		String cryptoPolicy = Security.getProperty("crypto.policy");
+		String javaHome = System.getProperty("java.home");
+
+		Path cryptoPolicyPath = null;
+
+		if (cryptoPolicy == null) {
+			cryptoPolicyPath = Paths.get(javaHome, "lib", "security");
+		}
+		else {
+			cryptoPolicyPath = Paths.get(
+				javaHome, "lib", "security", "policy", cryptoPolicy);
+		}
+
+		File exportPolicyJar = new File(
+			cryptoPolicyPath.toFile(), "US_export_policy.jar");
+		File localPolicyJar = new File(
+			cryptoPolicyPath.toFile(), "local_policy.jar");
+
+		if ((cryptoPolicy == null) &&
+			(!exportPolicyJar.exists() || !localPolicyJar.exists())) {
+
+			cryptoPolicyPath = Paths.get(
+				javaHome, "lib", "security", "policy", "unlimited");
+
+			exportPolicyJar = new File(
+				cryptoPolicyPath.toFile(), "US_export_policy.jar");
+			localPolicyJar = new File(
+				cryptoPolicyPath.toFile(), "local_policy.jar");
+		}
+
+		_logger.error("Crypto policy path: " + cryptoPolicyPath);
+
+		URL cipherURL = ClassLoader.getSystemResource(
+			"javax/crypto/Cipher.class");
+
+		_logger.error("Cipher URL: " + cipherURL);
+
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("Export policy JAR {file=");
+		sb.append(exportPolicyJar);
+		sb.append(", exists=");
+		sb.append(exportPolicyJar.exists());
+		sb.append("}");
+
+		_logger.error(sb.toString());
+
+		sb = new StringBuilder();
+
+		sb.append("Local policy JAR {file=");
+		sb.append(localPolicyJar);
+		sb.append(", exists=");
+		sb.append(localPolicyJar.exists());
+		sb.append("}");
+
+		_logger.error(sb.toString());
+
+		try {
+			return SSLContexts.createSystemDefault();
+		}
+		catch (Exception e) {
+			throw e;
+		}
 	}
 
 	private static final Logger _logger = LoggerFactory.getLogger(
