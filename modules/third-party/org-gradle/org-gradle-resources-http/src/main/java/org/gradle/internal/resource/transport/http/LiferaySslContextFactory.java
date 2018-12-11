5944167fb33c8635173d9f25396e4c9f187dab1a
Peter Shin <peter.shin@liferay.com>
2018-12-11 14:37:43 -0800

    LPS-88176 DEBUG CI

--- modules/third-party/org-gradle/org-gradle-resources-http/src/main/java/org/gradle/internal/resource/transport/http/LiferaySslContextFactory.java
+++ modules/third-party/org-gradle/org-gradle-resources-http/src/main/java/org/gradle/internal/resource/transport/http/LiferaySslContextFactory.java
@@ -14,12 +14,18 @@
 
 package org.gradle.internal.resource.transport.http;
 
+import java.io.File;
+
+import java.net.URL;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import java.security.Security;
+
 import javax.net.ssl.SSLContext;
 
 import org.apache.http.ssl.SSLContexts;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * @author Peter Shin
@@ -32,14 +38,49 @@ public class LiferaySslContextFactory extends DefaultSslContextFactory {
 			return super.createSslContext();
 		}
 
-		if (_logger.isInfoEnabled()) {
-			_logger.info("Creating default SSL context from system properties");
+		System.out.println(
+			"Creating default SSL context from system properties");
+
+		String cryptoPolicy = Security.getProperty("crypto.policy");
+		String javaHome = System.getProperty("java.home");
+
+		Path cryptoPolicyPath = null;
+
+		if (cryptoPolicy == null) {
+			cryptoPolicyPath = Paths.get(javaHome, "lib", "security");
 		}
+		else {
+			cryptoPolicyPath = Paths.get(
+				javaHome, "lib", "security", "policy", cryptoPolicy);
+		}
+
+		File exportJar = new File(
+			cryptoPolicyPath.toFile(), "US_export_policy.jar");
+		File importJar = new File(
+			cryptoPolicyPath.toFile(), "local_policy.jar");
+
+		if ((cryptoPolicy == null) &&
+			(!exportJar.exists() || !importJar.exists())) {
+
+			cryptoPolicyPath = Paths.get(
+				javaHome, "lib", "security", "policy", "unlimited");
+
+			exportJar = new File(
+				cryptoPolicyPath.toFile(), "US_export_policy.jar");
+			importJar = new File(cryptoPolicyPath.toFile(), "local_policy.jar");
+		}
+
+		URL jceCipherURL = ClassLoader.getSystemResource(
+			"javax/crypto/Cipher.class");
+
+		System.out.println("Crypto Policy Directory: " + cryptoPolicyPath);
+		System.out.println("Cipher Class URL: " + jceCipherURL);
+		System.out.println(
+			"Export JAR: " + exportJar + " (" + exportJar.exists() + ")");
+		System.out.println(
+			"Import JAR: " + importJar + " (" + importJar.exists() + ")");
 
 		return SSLContexts.createSystemDefault();
 	}
-
-	private static final Logger _logger = LoggerFactory.getLogger(
-		LiferaySslContextFactory.class);
 
 }
