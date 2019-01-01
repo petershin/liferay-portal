99733b80b382df9c88deb943a55756ea6f58aed0
Peter Shin <peter.shin@liferay.com>
2019-01-01 11:33:10 -0600

    LPS-87479 Create a digest from the sources

--- modules/sdk/gradle-plugins-node/src/main/java/com/liferay/gradle/plugins/node/tasks/NpmRunTask.java
+++ modules/sdk/gradle-plugins-node/src/main/java/com/liferay/gradle/plugins/node/tasks/NpmRunTask.java
@@ -14,17 +14,12 @@
 
 package com.liferay.gradle.plugins.node.tasks;
 
-import com.liferay.gradle.plugins.node.internal.util.GradleUtil;
+import com.liferay.gradle.plugins.node.internal.util.FileUtil;
 
 import java.io.File;
-import java.io.IOException;
 
 import java.nio.charset.StandardCharsets;
-import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.attribute.BasicFileAttributes;
 
 import java.util.Arrays;
 import java.util.LinkedHashSet;
@@ -33,7 +28,6 @@ import java.util.Set;
 
 import org.gradle.api.Project;
 import org.gradle.api.file.FileCollection;
-import org.gradle.api.logging.Logger;
 import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.InputDirectory;
 import org.gradle.api.tasks.InputFile;
@@ -50,8 +44,6 @@ public class NpmRunTask extends ExecuteNpmTask {
 	public NpmRunTask() {
 		Project project = getProject();
 
-		_reportFile = new File(getTemporaryDir(), "report.txt");
-
 		for (String pathname : _SOURCE_PATHNAMES) {
 			File sourceDir = new File(project.getProjectDir(), pathname);
 
@@ -63,71 +55,11 @@ public class NpmRunTask extends ExecuteNpmTask {
 
 	@Override
 	public void executeNode() throws Exception {
+		String sourceFilesDigest = FileUtil.getDigest(getSourceFiles());
+
 		super.executeNode();
 
-		StringBuilder sb = new StringBuilder();
-
-		sb.append("Executed npmRun");
-		sb.append(_scriptName);
-
-		File nodeModulesDir = getNodeModulesDir();
-
-		sb.append("\n\n=====Node Modules=====\n");
-		sb.append("Path: ");
-		sb.append(nodeModulesDir.getAbsolutePath());
-
-		for (File file : nodeModulesDir.listFiles()) {
-			String fileName = file.getName();
-
-			if (fileName.startsWith(".")) {
-				continue;
-			}
-
-			sb.append(fileName);
-			sb.append("\n");
-		}
-
-		File sourceDir = getSourceDir();
-
-		sb.append("\n\n=====Source Files=====\n");
-		sb.append("Path: ");
-		sb.append(sourceDir.getAbsolutePath());
-
-		try {
-			Files.walkFileTree(
-				sourceDir.toPath(),
-				new SimpleFileVisitor<Path>() {
-
-					@Override
-					public FileVisitResult visitFile(
-							Path file, BasicFileAttributes attrs)
-						throws IOException {
-
-						if (!attrs.isDirectory()) {
-							sb.append(file.getFileName());
-							sb.append("\n");
-						}
-
-						return FileVisitResult.CONTINUE;
-					}
-
-				});
-		}
-		catch (IOException ioe) {
-			Logger logger = getLogger();
-
-			logger.warn("Could not generate source files");
-		}
-
-		String report = sb.toString();
-
-		File reportFile = getReportFile();
-
-		Path path = reportFile.toPath();
-
-		Files.createDirectories(path.getParent());
-
-		Files.write(path, report.getBytes(StandardCharsets.UTF_8));
+		_writeDigest(sourceFilesDigest.getBytes(StandardCharsets.UTF_8));
 	}
 
 	@InputDirectory
@@ -144,14 +76,18 @@ public class NpmRunTask extends ExecuteNpmTask {
 		return project.file("package.json");
 	}
 
-	@OutputFile
-	public File getReportFile() {
-		return GradleUtil.toFile(getProject(), _reportFile);
-	}
-
 	@Input
 	public String getScriptName() {
 		return _scriptName;
+	}
+
+	@OutputFile
+	public File getSourceDigestFile() {
+		Project project = getProject();
+
+		String pathname = "npm/script/" + getName() + "/.digest";
+
+		return new File(project.getBuildDir(), pathname);
 	}
 
 	@InputFiles
@@ -197,11 +133,16 @@ public class NpmRunTask extends ExecuteNpmTask {
 		return completeArgs;
 	}
 
+	private void _writeDigest(byte[] bytes) throws Exception {
+		File file = getSourceDigestFile();
+
+		Files.write(file.toPath(), bytes);
+	}
+
 	private static final String[] _SOURCE_PATHNAMES = {
 		"src/main/resources/META-INF/resources", "test"
 	};
 
-	private final Object _reportFile;
 	private String _scriptName;
 	private final Set<Object> _sources = new LinkedHashSet<>();
 
