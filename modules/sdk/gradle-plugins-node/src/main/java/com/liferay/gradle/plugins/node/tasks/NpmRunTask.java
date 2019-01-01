51fb5d9b09f42507b94ed0eb52f146c87e0f2a84
Peter Shin <peter.shin@liferay.com>
2019-01-01 11:44:14 -0600

    LPS-87479 Update scriptName property for consistency with other gradle plugins

--- modules/sdk/gradle-plugins-node/src/main/java/com/liferay/gradle/plugins/node/tasks/NpmRunTask.java
+++ modules/sdk/gradle-plugins-node/src/main/java/com/liferay/gradle/plugins/node/tasks/NpmRunTask.java
@@ -97,7 +97,7 @@ public class NpmRunTask extends ExecuteNpmTask {
 
 	@Input
 	public String getScriptName() {
-		return _scriptName;
+		return GradleUtil.toString(_scriptName);
 	}
 
 	@InputFile
@@ -143,7 +143,7 @@ public class NpmRunTask extends ExecuteNpmTask {
 		_npmVersion = npmVersion;
 	}
 
-	public void setScriptName(String scriptName) {
+	public void setScriptName(Object scriptName) {
 		_scriptName = scriptName;
 	}
 
@@ -169,7 +169,7 @@ public class NpmRunTask extends ExecuteNpmTask {
 		List<String> completeArgs = super.getCompleteArgs();
 
 		completeArgs.add("run-script");
-		completeArgs.add(_scriptName);
+		completeArgs.add(getScriptName());
 
 		return completeArgs;
 	}
@@ -181,12 +181,12 @@ public class NpmRunTask extends ExecuteNpmTask {
 	}
 
 	private static final String[] _SOURCE_PATHNAMES = {
-		"src/main/resources/META-INF/resources", "test"
+		"src/main/resources", "test"
 	};
 
 	private Object _nodeVersion;
 	private Object _npmVersion;
-	private String _scriptName;
+	private Object _scriptName;
 	private final Set<Object> _sources = new LinkedHashSet<>();
 
 }
