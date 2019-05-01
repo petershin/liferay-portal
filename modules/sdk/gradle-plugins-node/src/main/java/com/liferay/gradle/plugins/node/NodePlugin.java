772b5ae377909c85c57053b94ca3a9431c65ed41
Peter Shin <peter.shin@liferay.com>
2019-05-01 09:47:44 -0700

    LPS-94947 Use Collections#emptyList for performance

--- modules/sdk/gradle-plugins-node/src/main/java/com/liferay/gradle/plugins/node/NodePlugin.java
+++ modules/sdk/gradle-plugins-node/src/main/java/com/liferay/gradle/plugins/node/NodePlugin.java
@@ -36,7 +36,7 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
@@ -447,7 +447,8 @@ public class NodePlugin implements Plugin<Project> {
 				rootProject, taskName, nodeExtension);
 		}
 
-		downloadNodeTask.setActions(Arrays.asList());
+		downloadNodeTask.setActions(Collections.emptyList());
+
 		downloadNodeTask.dependsOn(rootDownloadNodeTask);
 	}
 
