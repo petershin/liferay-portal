7457aa235d283dd96e5e07fea6123102c5253062
Peter Shin <peter.shin@liferay.com>
2019-08-29 10:05:27 -0700

    LPS-100515 Remove gradle-plugins-js-transpiler (apply)

--- modules/sdk/gradle-plugins-soy/src/main/java/com/liferay/gradle/plugins/soy/SoyPlugin.java
+++ modules/sdk/gradle-plugins-soy/src/main/java/com/liferay/gradle/plugins/soy/SoyPlugin.java
@@ -14,7 +14,6 @@
 
 package com.liferay.gradle.plugins.soy;
 
-import com.liferay.gradle.plugins.soy.internal.SoyPluginConstants;
 import com.liferay.gradle.plugins.soy.internal.util.GradleUtil;
 import com.liferay.gradle.plugins.soy.tasks.BuildSoyTask;
 import com.liferay.gradle.plugins.soy.tasks.WrapSoyAlloyTemplateTask;
@@ -115,7 +114,6 @@ public class SoyPlugin implements Plugin<Project> {
 		return buildSoyTask;
 	}
 
-	@SuppressWarnings("rawtypes")
 	private WrapSoyAlloyTemplateTask _addTaskWrapSoyAlloyTemplate(
 		Project project) {
 
@@ -132,18 +130,6 @@ public class SoyPlugin implements Plugin<Project> {
 			Collections.singleton("**/*.soy.js"));
 
 		PluginContainer pluginContainer = project.getPlugins();
-
-		pluginContainer.withId(
-			SoyPluginConstants.JS_TRANSPILER_PLUGIN_ID,
-			new Action<Plugin>() {
-
-				@Override
-				public void execute(Plugin plugin) {
-					wrapSoyAlloyTemplateTask.dependsOn(
-						SoyPluginConstants.TRANSPILE_JS_TASK_NAME);
-				}
-
-			});
 
 		pluginContainer.withType(
 			JavaPlugin.class,
