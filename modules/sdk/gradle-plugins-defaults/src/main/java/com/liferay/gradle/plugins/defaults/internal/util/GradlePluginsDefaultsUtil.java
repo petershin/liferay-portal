679703cf5bf34730821a00b2da76b6147df75f87
Peter Shin <peter.shin@liferay.com>
2020-12-14 10:32:36 -0600

    Revert "LRCI-1827 Auto SF"
    This reverts commit 2f0ae9c62113a64bfa42852bc09b029b84639ccf.

--- modules/sdk/gradle-plugins-defaults/src/main/java/com/liferay/gradle/plugins/defaults/internal/util/GradlePluginsDefaultsUtil.java
+++ modules/sdk/gradle-plugins-defaults/src/main/java/com/liferay/gradle/plugins/defaults/internal/util/GradlePluginsDefaultsUtil.java
@@ -31,6 +31,7 @@ import org.gradle.api.Project;
 import org.gradle.api.artifacts.dsl.RepositoryHandler;
 import org.gradle.api.artifacts.repositories.AuthenticationContainer;
 import org.gradle.api.artifacts.repositories.MavenArtifactRepository;
+import org.gradle.api.artifacts.repositories.MavenArtifactRepository.MetadataSources;
 import org.gradle.api.artifacts.repositories.PasswordCredentials;
 import org.gradle.internal.authentication.DefaultBasicAuthentication;
 
@@ -72,9 +73,8 @@ public class GradlePluginsDefaultsUtil {
 					public void execute(
 						MavenArtifactRepository mavenArtifactRepository) {
 
-						MavenArtifactRepository.MetadataSources
-							metadataSources =
-								mavenArtifactRepository.getMetadataSources();
+						MetadataSources metadataSources =
+							mavenArtifactRepository.getMetadataSources();
 
 						metadataSources.mavenPom();
 
