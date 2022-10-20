6ef9540c60741530d927cbda47eaf9b8f6f5c7fe
Seiphon Wang <seiphon.wang@liferay.com>
2022-10-20 18:09:59 +0800

    LPS-161876 Rename the project

---
+++ modules/util/language-security-scanner/src/main/java/com/liferay/language/security/scanner/util/AntiSamyUtil.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.language.security.scanner.util;
+
+import com.liferay.language.security.scanner.LanguageSecurityScanner;
+
+import java.net.URL;
+
+import org.owasp.validator.html.AntiSamy;
+import org.owasp.validator.html.CleanResults;
+import org.owasp.validator.html.Policy;
+import org.owasp.validator.html.PolicyException;
+import org.owasp.validator.html.ScanException;
+
+/**
+ * @author Seiphon Wang
+ */
+public class AntiSamyUtil {
+
+	public static String scan(String content)
+		throws PolicyException, ScanException {
+
+		ClassLoader classLoader =
+			LanguageSecurityScanner.class.getClassLoader();
+
+		URL antiSamyURL = classLoader.getResource("antisamy-liferay.xml");
+
+		String antiSamyPath = antiSamyURL.getFile();
+
+		Policy policy = Policy.getInstance(antiSamyPath);
+
+		AntiSamy antiSamy = new AntiSamy();
+
+		CleanResults cleanResults = antiSamy.scan(content, policy);
+
+		return cleanResults.getCleanHTML();
+	}
+
+}
