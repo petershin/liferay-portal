fbe0728a436f0c2340320f4708c3e6df483f8253
Alan Huang <alan.huang@liferay.com>
2022-12-06 09:44:51 +0800

    LPS-170150 Skip xml comment

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/XMLIndentationCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/XMLIndentationCheck.java
@@ -87,7 +87,8 @@ public class XMLIndentationCheck extends BaseFileCheck {
 
 			if (!s.startsWith("<") || s.startsWith("</") ||
 				s.startsWith("<![CDATA[") ||
-				XMLSourceUtil.isInsideCDATAMarkup(content, x)) {
+				XMLSourceUtil.isInsideCDATAMarkup(content, x) ||
+				XMLSourceUtil.isInsideComment(content, x)) {
 
 				continue;
 			}
@@ -101,7 +102,8 @@ public class XMLIndentationCheck extends BaseFileCheck {
 			String s = matcher.group(1);
 
 			if (s.equals("</code>") ||
-				XMLSourceUtil.isInsideCDATAMarkup(content, matcher.start())) {
+				XMLSourceUtil.isInsideCDATAMarkup(content, matcher.start()) ||
+				XMLSourceUtil.isInsideComment(content, matcher.start())) {
 
 				continue;
 			}
@@ -113,7 +115,9 @@ public class XMLIndentationCheck extends BaseFileCheck {
 		matcher = _incorrectLineBreakPattern2.matcher(content);
 
 		while (matcher.find()) {
-			if (XMLSourceUtil.isInsideCDATAMarkup(content, matcher.start())) {
+			if (XMLSourceUtil.isInsideCDATAMarkup(content, matcher.start()) ||
+				XMLSourceUtil.isInsideComment(content, matcher.start())) {
+
 				continue;
 			}
 
