4c0500e054cc054fa4bec35b1ba1c42b93cd32b2
Peter Shin <peter.shin@liferay.com>
2023-05-15 15:15:33 -0500

    LPS-181508 Copy

--- modules/sdk/gradle-plugins-theme-builder/src/main/java/com/liferay/gradle/plugins/theme/builder/ThemeBuilderPlugin.java
+++ modules/sdk/gradle-plugins-theme-builder/src/main/java/com/liferay/gradle/plugins/theme/builder/ThemeBuilderPlugin.java
@@ -31,6 +31,7 @@ import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.artifacts.DependencySet;
+import org.gradle.api.file.DuplicatesStrategy;
 import org.gradle.api.file.FileCollection;
 import org.gradle.api.file.FileCopyDetails;
 import org.gradle.api.file.FileTreeElement;
@@ -300,6 +301,7 @@ public class ThemeBuilderPlugin implements Plugin<Project> {
 
 			});
 
+		war.setDuplicatesStrategy(DuplicatesStrategy.INCLUDE);
 		war.setIncludeEmptyDirs(false);
 	}
 
