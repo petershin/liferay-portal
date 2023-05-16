316b8316d45eaa03872a226a74710a3e76b69253
Peter Shin <peter.shin@liferay.com>
2023-05-16 15:58:02 -0500

    LPS-181508 Hash

--- modules/sdk/gradle-plugins-node/src/main/java/com/liferay/gradle/plugins/node/internal/util/DigestUtil.java
+++ modules/sdk/gradle-plugins-node/src/main/java/com/liferay/gradle/plugins/node/internal/util/DigestUtil.java
@@ -15,6 +15,8 @@
 package com.liferay.gradle.plugins.node.internal.util;
 
 import com.liferay.gradle.util.Validator;
+import com.liferay.gradle.util.hash.HashUtil;
+import com.liferay.gradle.util.hash.HashValue;
 
 import java.io.File;
 import java.io.IOException;
@@ -33,8 +35,6 @@ import java.util.TreeSet;
 
 import org.gradle.api.GradleException;
 import org.gradle.api.UncheckedIOException;
-import org.gradle.internal.hash.HashUtil;
-import org.gradle.internal.hash.HashValue;
 
 /**
  * @author Hugo Huijser
