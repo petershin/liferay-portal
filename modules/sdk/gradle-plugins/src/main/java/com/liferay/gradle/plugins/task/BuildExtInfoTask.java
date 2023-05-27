963552e4e5d2a947bc2558c456b149eb4f52cf0b
Seiphon Wang <seiphon.wang@liferay.com>
2023-05-27 21:14:47 +0800

    LPS-181508 Fix tests issues

--- modules/sdk/gradle-plugins/src/main/java/com/liferay/gradle/plugins/task/BuildExtInfoTask.java
+++ modules/sdk/gradle-plugins/src/main/java/com/liferay/gradle/plugins/task/BuildExtInfoTask.java
@@ -59,6 +59,8 @@ public class BuildExtInfoTask extends JavaExec {
 		return GradleUtil.toFile(getProject(), _baseDir);
 	}
 
+	@InputDirectory
+	@PathSensitive(PathSensitivity.RELATIVE)
 	public File getOutputDir() {
 		return GradleUtil.toFile(getProject(), _outputDir);
 	}
@@ -78,7 +80,6 @@ public class BuildExtInfoTask extends JavaExec {
 		_baseDir = baseDir;
 	}
 
-	@Input
 	public void setOutputDir(Object outputDir) {
 		_outputDir = outputDir;
 	}
