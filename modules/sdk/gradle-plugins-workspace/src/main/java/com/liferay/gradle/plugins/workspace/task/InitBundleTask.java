f74882cef25e137b0fe6d67daf8c3d2817cdcd0a
Seiphon Wang <seiphon.wang@liferay.com>
2023-05-27 21:14:47 +0800

    LPS-181508 Fix tests issues

--- modules/sdk/gradle-plugins-workspace/src/main/java/com/liferay/gradle/plugins/workspace/task/InitBundleTask.java
+++ modules/sdk/gradle-plugins-workspace/src/main/java/com/liferay/gradle/plugins/workspace/task/InitBundleTask.java
@@ -36,6 +36,7 @@ import org.gradle.api.logging.Logger;
 import org.gradle.api.provider.Property;
 import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.InputFile;
+import org.gradle.api.tasks.InputFiles;
 import org.gradle.api.tasks.JavaExec;
 import org.gradle.api.tasks.Optional;
 import org.gradle.api.tasks.OutputDirectory;
@@ -71,7 +72,7 @@ public class InitBundleTask extends JavaExec {
 		return GradleUtil.toString(_configEnvironment);
 	}
 
-	@Optional
+	@InputFiles
 	public File getConfigsDir() {
 		return GradleUtil.toFile(getProject(), _configsDir);
 	}
@@ -86,14 +87,13 @@ public class InitBundleTask extends JavaExec {
 		return GradleUtil.toFile(getProject(), _file);
 	}
 
-	@Input
+	@InputFiles
 	@Optional
 	public FileCollection getProvidedModules() {
 		return _providedModules;
 	}
 
 	@Input
-	@Optional
 	public int getStripComponents() {
 		return GradleUtil.toInteger(_stripComponents);
 	}
