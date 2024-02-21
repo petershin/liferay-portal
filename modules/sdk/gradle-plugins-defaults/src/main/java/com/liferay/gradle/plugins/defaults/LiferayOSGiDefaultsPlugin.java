24568aa2894e89d38440fa01e76b99b9965740d7
Peter Shin <peter.shin@liferay.com>
2024-02-21 14:25:32 -0600

    LPD-15162 Override JDK

--- modules/sdk/gradle-plugins-defaults/src/main/java/com/liferay/gradle/plugins/defaults/LiferayOSGiDefaultsPlugin.java
+++ modules/sdk/gradle-plugins-defaults/src/main/java/com/liferay/gradle/plugins/defaults/LiferayOSGiDefaultsPlugin.java
@@ -2712,8 +2712,17 @@ public class LiferayOSGiDefaultsPlugin implements Plugin<Project> {
 		JavaPluginConvention javaPluginConvention = GradleUtil.getConvention(
 			project, JavaPluginConvention.class);
 
-		javaPluginConvention.setSourceCompatibility(_JAVA_VERSION);
-		javaPluginConvention.setTargetCompatibility(_JAVA_VERSION);
+		if (project.hasProperty("build.java.override.version")) {
+			String javaVersion = GradleUtil.getProperty(
+				project, "build.java.override.version", (String)null);
+
+			javaPluginConvention.setSourceCompatibility(javaVersion);
+			javaPluginConvention.setTargetCompatibility(javaVersion);
+		}
+		else {
+			javaPluginConvention.setSourceCompatibility(_JAVA_VERSION);
+			javaPluginConvention.setTargetCompatibility(_JAVA_VERSION);
+		}
 
 		File testResultsDir = project.file("test-results/unit");
 
