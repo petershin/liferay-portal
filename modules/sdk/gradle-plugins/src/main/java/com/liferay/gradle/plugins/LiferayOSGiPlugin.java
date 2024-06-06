dc98527a1873c1cd5f668bbbf9f2954fb1a902d8
Peter Shin <peter.shin@liferay.com>
2024-06-06 10:16:41 -0500

    LPD-26276 SF

--- modules/sdk/gradle-plugins/src/main/java/com/liferay/gradle/plugins/LiferayOSGiPlugin.java
+++ modules/sdk/gradle-plugins/src/main/java/com/liferay/gradle/plugins/LiferayOSGiPlugin.java
@@ -17,7 +17,6 @@ import aQute.bnd.version.Version;
 
 import aQute.lib.utf8properties.UTF8Properties;
 
-import com.liferay.gradle.plugins.css.builder.CSSBuilderPlugin;
 import com.liferay.gradle.plugins.extensions.BundleExtension;
 import com.liferay.gradle.plugins.extensions.LiferayExtension;
 import com.liferay.gradle.plugins.extensions.LiferayOSGiExtension;
